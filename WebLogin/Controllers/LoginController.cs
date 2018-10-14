1488b54a7a066dad56e4c0dad2c5578e837436ae
Carlos May <Carlos May@WIN-6L1CH3UJM9G>
2018-10-14 17:36:25 -0500

    Agregar archivos de proyecto.

---
+++ WebLogin/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebLogin.Models;
+
+namespace WebLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autorizar(WebLogin.Models.Usuario userModel)
+        {
+            using (LoginDBEntities db = new LoginDBEntities())
+            {
+                var usuarioDet = db.Usuarios.Where(x => x.Usuario1 == userModel.Usuario1 && x.Password == userModel.Password).FirstOrDefault();
+                if(usuarioDet == null)
+                {
+                    userModel.MensajeDeError = "Usuario o PW incorrectos";
+                    return View("Index",userModel);
+                }
+                else
+                {
+                    Session["IdUsuario"] = userModel.Usuario1;
+                    return RedirectToAction("Index", "Usuario");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ WebLogin/Controllers/UsuarioController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebLogin.Models;
+
+namespace WebLogin.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            try
+            {
+                using (LoginDBEntities db = new LoginDBEntities())
+                {
+                    List<Usuario> lista = db.Usuarios.Where(a => a.Estatus.Value).ToList();
+                    return View(lista);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Agregar(Usuario rowUsuario)
+        {
+            if(!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (LoginDBEntities db = new LoginDBEntities())
+                {
+                    var usuarioExist = db.Usuarios.Where(x => x.Usuario1 == rowUsuario.Usuario1 && x.Correo == rowUsuario.Correo).FirstOrDefault();
+                    if (!(usuarioExist == null))
+                    {
+                        ModelState.AddModelError("", "Ya existe un usuario con la misma información");
+                        return View();
+                    }
+
+                    db.Usuarios.Add(rowUsuario);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("","Error al agregar usuario" + ex.Message);
+                return View();
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            try
+            {
+                using (LoginDBEntities db = new LoginDBEntities())
+                {
+                    Usuario usuario = db.Usuarios.Find(id);
+                    return View(usuario);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Usuario rowUsuario)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View();
+                using (LoginDBEntities db = new LoginDBEntities())
+                {
+                    Usuario usuarioReg = db.Usuarios.Find(rowUsuario.Id);
+                    usuarioReg.Correo = rowUsuario.Correo;
+                    usuarioReg.Sexo = rowUsuario.Sexo;
+                    usuarioReg.Password = rowUsuario.Password;
+                    usuarioReg.ConfirmaPassword = rowUsuario.Password;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error al editar usuario" + ex.Message);
+                return View();
+            }
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            try
+            {
+                using (LoginDBEntities db = new LoginDBEntities())
+                {
+                    Usuario usuario = db.Usuarios.Find(id);
+                    usuario.Estatus = false;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
